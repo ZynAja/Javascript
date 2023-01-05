261d165e77eac61315e54fd1e10c1a18c8f56fdb
Mohamad.Rizki.S <110074153+ZynAja@users.noreply.github.com>
2023-01-05 08:31:03 +0700

    Add files via upload

---
+++ Javascript 1-20/js/eventlistener.js
@@ -0,0 +1,21 @@
+function coba() {
+    a = document.querySelector(".isi");
+    a.innerHTML = "belajar eventlistener";
+    console.log("coba eventlistener");
+}
+
+// judul.addEventListener("click", coba);
+
+// judul.onmouseover =coba;
+
+judul.onmouseover = function () {
+    console.log("coba function anonymous");
+}
+
+
+
+
+
+
+
+
