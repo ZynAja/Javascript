f7470ef6826d98f97cde69736148725def81c8fa
Mohamad.Rizki.S <110074153+ZynAja@users.noreply.github.com>
2023-01-24 07:48:03 +0700

    Add files via upload

---
+++ 31-32/js/array.js
@@ -0,0 +1,40 @@
+let coba = function () {
+    return "coba function";
+}
+
+let buah = ["apel", "jeruk", "mangga", 10, coba(),
+    tes = ()=>"hasil return arrow function",
+    function nama() {
+        return "ZynAja";
+    },
+];
+
+console.log(buah);
+
+console.log(buah[0]);
+
+for(let i in buah){
+    console.log(buah[i]);
+}
+
+console.log(buah[5]());
+
+console.log(buah[6]());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
