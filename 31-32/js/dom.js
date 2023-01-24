1cfb56ca26f39d1f39c366fe3d24faca57febd2b
Mohamad.Rizki.S <110074153+ZynAja@users.noreply.github.com>
2023-01-24 07:49:01 +0700

    Delete 31-32 directory

--- 31-32/js/dom.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("h1").innerText="saya belajar javascript";
-
-document.querySelector("p").innerText="belajar javascript itu mudah";
-
-document.querySelector(".isi").innerText="saya belajar javascript smk di smk revit";
-
-document.querySelector("#judul").innerText="belajar javascript itu menyenangkan";
-
-
